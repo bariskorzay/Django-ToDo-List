b4b2710cba0101c96ce5adddb6751dc4f6ecd63d
Barış Korzay <bariskorzay@gmail.com>
2023-06-20 23:11:05 +0300

    feat: Add part 6-7

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'completed', 'priority')
+    list_filter = ('created_at',)
+    search_fields = ('title', 'description')
+    list_editable = ('completed',)
+    fieldsets = (
+        ('Görev Detayları', {
+            'fields': ('title', 'description', 'priority')
+        }),
+    )
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+
+    PRIORITY_CHOICES = [
+        ('low', 'Düşük'),
+        ('medium', 'Orta'),
+        ('high', 'Yüksek'),
+    ]
+
+    title = models.CharField(max_length=200, null=False,
+                             blank=False, verbose_name='Başlık')
+    description = models.TextField(
+        null=True, blank=True, verbose_name='Açıklama')
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name='Oluşturulma Tarihi')
+    completed = models.BooleanField(
+        default=False, verbose_name='Tamamlandı mı?')
+    priority = models.CharField(
+        max_length=20, choices=PRIORITY_CHOICES, default='low', verbose_name='Öncelik')
